b744bfdc70f605176f49be3143355fb7751f611e
Ponnusamy, Vijay <vijay.ponnusamy@outlook.com>
2020-05-02 22:05:25 +0530

    Changes to include employment tax and options

--- ScanDemo.java
+++ ScanDemo.java
@@ -8,9 +8,28 @@ class ScanDemo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please Enter your Annual Salary :");
-        double professionalTax = 2400.0D, PF=12115.0D, section80c = 150000.0D, standardDeduction = 40000.0D,
-                houseLoanInt = 200000.0D;
+        double professionalTax = 2496.0D, PF=0D, section80c = 150000.0D, standardDeduction = 50000.0D,
+        houseLoanInt = 0D, employmentTax=2500.0D;
         double annualSalary = scan.nextDouble();
+        scan.nextLine();
+        System.out.println("Are you paying interest on your homeloan?");
+        String homeLoanind = scan.nextLine().toUpperCase();
+        if (homeLoanind.equals("Y") || homeLoanind.equals("YES")){
+            System.out.println("What is you interest component for the whole year?");
+            houseLoanInt = scan.nextDouble();
+            if (houseLoanInt > 200000) houseLoanInt = 200000.0D;
+            if (houseLoanInt < 200000) houseLoanInt = 0.0D;
+        }
+        System.out.println("Do you know your PF deduction?");
+        String PFind = scan.nextLine().toUpperCase();
+        if (PFind.equals("Y") || PFind.equals("YES")){
+            System.out.println("What is your monthly PF deduction?");
+            PF = scan.nextDouble();
+            if (PF < 0) PF = 0.0D;
+        }
+        else{
+            PF = (annualSalary * 0.4 *0.12)/12;
+        }
         double totalTax = 0.0D, remainingSalary = 0.0D;
         int[] salarySlab = new int[]{250000, 250000, 500000, 1000000};
         double[] taxRate = new double[]{0.0D, 5.2D, 20.8D, 31.2D};
@@ -18,7 +37,7 @@ class ScanDemo {
         double[] taxRate2020 = new double[]{0.0D, 5.2D, 10.4D, 15.6D, 20.8D, 26.0D, 31.2D};
 
         DecimalFormat ft = new DecimalFormat("Rs ##,##,###.##");
-        remainingSalary = annualSalary - section80c - standardDeduction - houseLoanInt;
+        remainingSalary = annualSalary - section80c - standardDeduction - houseLoanInt - employmentTax;
 
         for(int i = 0; i < 4 && annualSalary > 0.0D; ++i) {
             totalTax += (double)salarySlab[i] * taxRate[i] / 100.0D;
@@ -29,9 +48,12 @@ class ScanDemo {
         }
 
         System.out.println(" ");
-        System.out.println("================= SALARY COMPUTATION WITH DEDUCTIONS ===================");
+        System.out.println("================= SALARY COMPUTATION WITH DEDUCTIONS (Old) ===================");
         System.out.println("Your annual salary is         : " + ft.format(annualSalary));
-        System.out.println("Your total tax liability is   : " + ft.format(totalTax));
+        System.out.println("Your annual tax liability is  : " + ft.format(totalTax));
+        System.out.println("Your monthly salary is        : " + ft.format(annualSalary/12.0));
+        System.out.println("Your monthly tax liability is : " + ft.format(totalTax/12.0));
+        System.out.println("Your monthly PF Contribution  : " + ft.format(PF));
         System.out.println("Your net monthly take home is : " +
                 ft.format((annualSalary - totalTax - professionalTax - (PF*12)) / 12.0D));
         System.out.println(" ");
@@ -45,9 +67,12 @@ class ScanDemo {
             }
         }
         System.out.println(" ");
-        System.out.println("================= SALARY COMPUTATION WITHOUT DEDUCTIONS ===================");
+        System.out.println("================= SALARY COMPUTATION WITHOUT DEDUCTIONS (New) ===================");
         System.out.println("Your annual salary is         : " + ft.format(annualSalary));
-        System.out.println("Your total tax liability is   : " + ft.format(totalTax));
+        System.out.println("Your annual tax liability is  : " + ft.format(totalTax));
+        System.out.println("Your monthly salary is        : " + ft.format(annualSalary/12.0));
+        System.out.println("Your monthly tax liability is : " + ft.format(totalTax/12.0));
+        System.out.println("Your monthly PF Contribution  : " + ft.format(PF));
         System.out.println("Your net monthly take home is : " +
                 ft.format((annualSalary - totalTax - professionalTax - (PF*12)) / 12.0D));
 
